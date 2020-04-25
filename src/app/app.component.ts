0b1b729aa143cad30b0c1a25e8ca4ca409b74a93
ibadullaevn <40333337+ibadullaevn@users.noreply.github.com>
2020-04-26 02:56:21 +0600

    auth(jwt)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from './services/category.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'online-shop';
+  logged = false;
+  username = '';
+  password = ''
+  constructor(private categoryService: CategoryService) {
+  }
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.logged = true;
+    }
+  }
+
+  login() {
+    this.categoryService.login(this.username, this.password)
+      .subscribe(res => {
+
+        localStorage.setItem('token', res.token);
+
+        this.logged = true;
+
+        this.username = '';
+        this.password = '';
+      });
+  }
+  logout() {
+    localStorage.clear();
+    this.logged = false;
+  }
+
 }
